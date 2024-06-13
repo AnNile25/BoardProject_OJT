7f240356ad7d0132f441bcbc39c83efa13248d10
이한나 <150889773+AnNile25@users.noreply.github.com>
2024-06-13 17:04:05 +0900

    Delete PrjBoard directory
    깃 삭제 후 재 등록

--- PrjBoard/src/main/java/com/gaea/work/cmn/GLog.java
+++
@@ -1,9 +0,0 @@
-package com.gaea.work.cmn;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public interface GLog {
-
-	Logger LOG = LogManager.getLogger(GLog.class);
-}
