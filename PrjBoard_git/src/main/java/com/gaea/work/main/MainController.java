e340d804d7f6ec26b224739b5cd78f875b6691e2
NILE\dlgks <dlgkssk1919@gmail.com>
2024-06-14 10:01:23 +0900

    comment(MainController): 주석 삭제

--- PrjBoard_git/src/main/java/com/gaea/work/main/MainController.java
+++ PrjBoard_git/src/main/java/com/gaea/work/main/MainController.java
@@ -3,20 +3,13 @@ package com.gaea.work.main;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.gaea.work.cmn.GLog;
-
 @Controller
 @RequestMapping("main")
-public class MainController implements GLog {
+public class MainController  {
 	public MainController() {}
 	
 	@RequestMapping(value="/mainView.do")
 	public String mainView() {
-		String view ="main/main";
-		LOG.debug("┌───────────────────────────────────────────┐");
-		LOG.debug("│ mainView                                  │");
-		LOG.debug("└───────────────────────────────────────────┘");	
-		
-		return view;
+		return "main/main";
 	}
 }
